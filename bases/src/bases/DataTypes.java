8d618f8d04639e1a73f41f92e769ce50d48f8a13
GuidoDev04 <lescanogui08@gmail.com>
2025-09-29 10:16:39 +0200

    Examples of data types on Java.

---
+++ bases/src/bases/DataTypes.java
@@ -0,0 +1,48 @@
+package bases;
+
+import java.math.BigDecimal;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		System.out.println("We can use Unicode for almost everything: 😊");
+		
+		char character = 'à';
+		System.out.println(character);
+		
+		double d1 = 0.1;
+		double d2 = 0.2;
+		
+		double sum = d1 + d2;
+		
+		System.out.printf("%.2f\n", sum);
+		System.out.println(sum);
+		
+	//	double dPrimitivo = null;
+		Double dEncapsulado = null;
+		
+		System.out.println(dEncapsulado);
+		
+		String textWithNumber = "1234";
+		
+		int number = Integer.parseInt(textWithNumber);
+		
+		System.out.println(number * 2);
+		
+		BigDecimal bd1 = new BigDecimal("0.1");
+		BigDecimal bd2 = new BigDecimal("0.2");
+		
+		System.out.println(bd1);
+		System.out.println(bd2);
+		
+		BigDecimal bdSum = bd1.add(bd2);
+		
+		System.out.println(bdSum);
+		
+		
+		
+		
+		
+		
+	
+}
+}
