fc4a4c91893e0cf9d46985f5cd5208ba7997063e
GuidoDev04 <lescanogui08@gmail.com>
2025-10-06 16:00:56 +0200

    Collections Example

---
+++ bases/src/bases/Collections.java
@@ -0,0 +1,21 @@
+package bases;
+
+import java.util.ArrayList;
+
+public class Collections {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		list.add(5);
+		list.add(6);
+		
+		System.out.println(list.get(1));
+		
+		for(int value: list) {
+			System.out.println(value);
+		}
+		
+		list.forEach(System.out::println);
+	}
+
+}
