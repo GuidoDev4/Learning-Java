e9bfad52fcb9462f9a6bb54b854b00dd1b1766e2
GuidoDev04 <lescanogui08@gmail.com>
2025-10-06 16:00:08 +0200

    More examples for use of Arrays

--- bases/src/bases/ArraysExamples.java
+++ bases/src/bases/ArraysExamples.java
@@ -14,5 +14,13 @@ public class ArraysExamples {
 		System.out.println(arr.length);
 		
 		System.out.println(Arrays.toString(arr));
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		for(int value: arr) {
+			System.out.println(value);
+		}
 }
 }
